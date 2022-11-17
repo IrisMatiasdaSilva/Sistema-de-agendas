3881303d956165b315bb8ec4642af0b1b3f948df
Íris Matias da Silva <112780817+IrisMatiasdaSilva@users.noreply.github.com>
2022-11-17 19:33:38 -0300

    Delete agenda.h

--- agenda.h
+++
@@ -1,9 +0,0 @@
-typedef struct passagens Passagens;
-
-typedef  struct lista Lista;
-
-void ler(Agendas *a);
-
-void inserir_inicio(Lista* plista, Agendas dado);
-
-int Consultar_quantitativo(Lista lista);
