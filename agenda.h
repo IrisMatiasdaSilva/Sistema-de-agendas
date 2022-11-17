3c27cb65f7c6e0bc852d78af8178ada473a8bfcf
IrisMatiasdaSilva <112780817+IrisMatiasdaSilva@users.noreply.github.com>
2022-11-17 06:52:22 -0300

    Update agenda.h

---
+++ agenda.h
@@ -0,0 +1,9 @@
+typedef struct passagens Passagens;
+
+typedef  struct lista Lista;
+
+void ler(Agendas *a);
+
+void inserir_inicio(Lista* plista, Agendas dado);
+
+int Consultar_quantitativo(Lista lista);
